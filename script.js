4a7e53c678e0e91c70e68c90b851f4c92514d36c
PIeter <pieter_ophalvens@hotmail.com>
2023-11-14 16:56:23 +0100

    styling,margins  and daily info

--- script.js
+++ script.js
@@ -24,18 +24,17 @@ function weather(){
     main.innerHTML=[];
     console.log(data);
     let location=document.createElement("div");
+    location.style.padding="20px";
     let time=new Date(data.location.localtime);
     location.textContent=data.location.name+', '+data.location.country+", Today "+daysofweek[time.getDay()]+" "+time.getDate()+" "+months[time.getMonth()]+" "+time.getHours()+":"+time.getMinutes();
     let weather=document.createElement("p");
     weather.textContent=data.current.condition.text;
     let weatherimgdiv=document.createElement("div");
-    weatherimgdiv.style.paddingLeft="50px";
-    weatherimgdiv.style.paddingRight="20px";
+    weatherimgdiv.style.padding="20px";
     let weatherimg=document.createElement("img");
     weatherimg.src=data.current.condition.icon;
     let temperature=document.createElement("div");
-    temperature.style.paddingLeft="10px";
-    temperature.style.paddingRight="10px";
+    temperature.style.padding="20px";
     let text=document.createElement("p");
     text.textContent="Temp: "+data.current.temp_c+"°C";
     let rain=document.createElement("p");
@@ -43,31 +42,47 @@ function weather(){
     let datacurrenthour=data.forecast.forecastday[0].hour[currenthour];
     rain.textContent="Rain: "+datacurrenthour.chance_of_rain+"%";
     let wind=document.createElement("p");
-    wind.textContent="Wind: "+data.current.gust_kph+"kph";
+    wind.textContent="Wind: "+data.current.wind_kph+"kph";
     
     let extrainfo=document.createElement("div");
-    extrainfo.style.paddingLeft="20px";
-    extrainfo.style.paddingRight="20px";
+    extrainfo.style.padding="20px";
     let texti=document.createElement("p");
     texti.textContent="Feels like: "+datacurrenthour.feelslike_c+"°C";
     let raini=document.createElement("p");
-    raini.textContent="Humidity: "+datacurrenthour.humidity;
+    raini.textContent="precip: "+datacurrenthour.precip_mm+"mm";
     let windi=document.createElement("p");
     console.log(datacurrenthour);
-    windi.textContent="Snow: "+datacurrenthour.chance_of_snow+"%";
+    windi.textContent="Wind direction: "+datacurrenthour.wind_dir;
+    
+    let extraextrainfo=document.createElement("div");
+    extraextrainfo.style.padding="20px";
+    let textii=document.createElement("p");
+    textii.textContent="UV index: "+datacurrenthour.uv;
+    let rainii=document.createElement("p");
+    rainii.textContent="Humidity: "+datacurrenthour.humidity+"%";
+    let windii=document.createElement("p");
+    console.log(datacurrenthour);
+    windii.textContent="Snow: "+datacurrenthour.chance_of_snow+"%";
     
     main.appendChild(location);
     weatherimgdiv.appendChild(weather);
     weatherimgdiv.appendChild(weatherimg);
     main.appendChild(weatherimgdiv);
+
     temperature.appendChild(text);
     temperature.appendChild(rain);
     temperature.appendChild(wind);
     main.appendChild(temperature);
+
     extrainfo.appendChild(texti);
     extrainfo.appendChild(raini);
     extrainfo.appendChild(windi);
     main.appendChild(extrainfo);
+
+    extraextrainfo.appendChild(textii);
+    extraextrainfo.appendChild(rainii);
+    extraextrainfo.appendChild(windii);
+    main.appendChild(extraextrainfo);
     
   })
   .catch(error => {
@@ -78,15 +93,12 @@ function forecast(){
   fetch(('https://api.weatherapi.com/v1/forecast.json?key=e42f2703bee5405092192213231311&q='+loadedlocation+'&days=10&aqi=no&alerts=no'))
   .then(response => response.json())
   .then(data => {
-    // console.log(data);
     let main=document.body.querySelector(".forecast");
     main.innerHTML=[];
-    
-    // console.log(data.forecast.forecastday[0]);
     data.forecast.forecastday.forEach(element => {
       let forecastday=document.createElement("div");
       forecastday.classList.add("day");
-      forecastday.style.backgroundImage="./forecastexample.png";
+      forecastday.style.padding="20px";
       const day=new Date(element.date);
       forecastday.textContent=daysofweek[day.getDay()]+" "+day.getDate() +" "+ months[day.getMonth()];
       let weather=document.createElement("div");
